5676e657894d0eed9d1563b89d863a5345f653c1
Phạm Nguyễn <113409512+JumpToTheSky@users.noreply.github.com>
2025-06-09 17:58:19 +0700

    update

--- assets/Script/RoomScene/Input/InputController.js
+++ assets/Script/RoomScene/Input/InputController.js
@@ -1,41 +1,66 @@
-// Learn cc.Class:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
-
+const GameConfig = require('GameConfig');
+const Emitter = require('Emitter');
+const EventKey = require('EventKey');
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        moveUpButton: {
+            default: null,
+            type: cc.Button,
+        },
+        moveDownButton: {
+            default: null,
+            type: cc.Button,
+        },
+        skillButton: {
+            default: null,
+            type: cc.Button,
+        },
+        bombButton: {
+            default: null,
+            type: cc.Button,
+        },
+        cooldownPrefab: {
+            default: null,
+            type: cc.Prefab,
+        },
     },
-
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {},
-
-    start () {
-
+    onLoad() {
+        this.setCooldown();
+        this.registerButtonEvents();
     },
+    registerButtonEvents() {
+        this.moveUpButton.node.on('click', this.onMoveUp, this);
+        this.moveDownButton.node.on('click', this.onMoveDown, this);
+        this.skillButton.node.on('click', this.onUseSkill, this);
+        this.bombButton.node.on('click', this.onUseBomb, this);
+    },
+    setCooldown() {
+        this.skillCooldown = cc.instantiate(this.cooldownPrefab);
+        skillCooldown.getComponent('CooldownController').durationSeconds = GameConfig.BULLET.TYPE.ULTIMATE.COOLDOWN;
+        skillCooldown.parent = this.node.getChildByName('SkillInput');
 
-    // update (dt) {},
+        this.bombCooldown = cc.instantiate(this.cooldownPrefab);
+        bombCooldown.getComponent('CooldownController').durationSeconds = GameConfig.SHOP.ITEM.BOMB.COOLDOWN;
+        bombCooldown.parent = this.node.getChildByName('BombInput');    
+    },
+    onMoveUp() {
+        Emitter.emit(EventKey.INPUT.MOVE_UP);
+    },
+    onMoveDown() {
+        Emitter.emit(EventKey.INPUT.MOVE_DOWN);
+    },
+    onUseSkill() {
+        Emitter.emit(EventKey.INPUT.SHOOT_ULTIMATE);
+    },  
+    onUseBomb() {
+        Emitter.emit(EventKey.INPUT.USE_BOMB);
+    },
+    onDestroy() {
+        this.moveUpButton.node.off('click', this.onMoveUp, this);
+        this.moveDownButton.node.off('click', this.onMoveDown, this);
+        this.skillButton.node.off('click', this.onUseSkill, this);
+        this.bombButton.node.off('click', this.onUseBomb, this);
+    },
 });
