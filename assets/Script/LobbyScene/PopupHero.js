91566097fbe7cfbc4eea329f29cdfcc6fcfb3af0
NTNhan0626 <ntnhan0626@gmail.com>
2025-06-10 18:10:45 +0700

    handle get data from gameconfig and localstorage

--- assets/Script/LobbyScene/PopupHero.js
+++ assets/Script/LobbyScene/PopupHero.js
@@ -1,87 +1,147 @@
 const ButtonName = require('ButtonName');
+const GameConfig = require('GameConfig');
+const UpgradeController = require('UpgradeController');
 cc.Class({
     extends: require('PopupItem'),
 
     properties: {
-        activeSpriteFrame:{
-            type:cc.SpriteFrame,
-            default:null
+        activeSpriteFrame: {
+            type: cc.SpriteFrame,
+            default: null
         },
-        unActiveSpriteFrame:{
-            type:cc.SpriteFrame,
-            default:null
+        unActiveSpriteFrame: {
+            type: cc.SpriteFrame,
+            default: null
         },
         statsButton: {
-            type:cc.Node,
-            default:null
+            type: cc.Node,
+            default: null
         },
         skillButton: {
-            type:cc.Node,
-            default:null
+            type: cc.Node,
+            default: null
         },
         stats: {
-            type:cc.Node,
-            default:null
+            type: cc.Node,
+            default: null
         },
-        skill:{
-            type:cc.Node,
-            default:null
-        }
+        skill: {
+            type: cc.Node,
+            default: null
+        },
+        hpBase: {
+            type: cc.Label,
+            default: null
+        },
+        damageBase: {
+            type: cc.Label,
+            default: null
+        },
+        attackSpeed: {
+            type: cc.Label,
+            default: null
+        },
+        leverNomalAttack: {
+            type: cc.Label,
+            default: null
+        },
+        leverUltimate: {
+            type: cc.Label,
+            default: null
+        },
+        damageNomalAttack: {
+            type: cc.Label,
+            default: null
+        },
+        damageUltimate: {
+            type: cc.Label,
+            default: null
+        },
+        priceUpgradeNomalAttack:{
+            type: cc.Label,
+            default: null
+        },
+        priceUpgradeUltimate:{
+            type: cc.Label,
+            default: null
+        },
+
     },
-    onLoad(){
+    onLoad() {
         this.init();
     },
-    init(){
-        
+    init() {
         this.stats.active = false;
         this.skill.active = false;
         this.activeNode(ButtonName.STATS);
+
+        this.hpBase.string = GameConfig.PLAYER.HP_BASE.toString();
+        this.damageBase.string = GameConfig.BULLET.DAMAGE_BASE.toString();
+        this.attackSpeed.string = GameConfig.BULLET.TYPE.NOMAL.COOLDOWN.toString();
+
+        this.currentLeverNomalAttack = UpgradeController.getLeverNomalAttack();
+        this.currentLeverUltimate = UpgradeController.getLeverUltimate();
+        this.leverNomalAttack.string = this.currentLeverNomalAttack.toString();
+        this.leverUltimate.string = this.currentLeverUltimate.toString();
+
+        let dameBase = GameConfig.BULLET.DAMAGE_BASE;
+        let coefficientNomalDame = GameConfig.BULLET.TYPE.NOMAL.COEFFICIENT_DAMAGE;
+        let coefficientUltimateDame = GameConfig.BULLET.TYPE.ULTIMATE.COEFFICIENT_DAMAGE;
+        let percentNomalDameAdd = GameConfig.BULLET.TYPE.NOMAL.UPGRADE.PERCENT_DAMAGE_ADD;
+        let percentUltimateDameAdd = GameConfig.BULLET.TYPE.ULTIMATE.UPGRADE.PERCENT_DAMAGE_ADD;
+
+        this.damageNomalAttack.string = Math.floor((dameBase*coefficientNomalDame*(1 + this.currentLeverNomalAttack*percentNomalDameAdd))).toString();
+        this.damageUltimate.string = Math.floor((dameBase*coefficientUltimateDame*(1 + this.currentLeverUltimate*percentUltimateDameAdd))).toString();
+
+
+
+
+
     },
-    onButtonClick(event,data){
+    onButtonClick(event, data) {
         this.activeNode(data);
         this.activeButton(data);
     },
-    activeNode(name){
+    activeNode(name) {
         this.stats.opacity = 0;
         this.skill.opacity = 0;
-        if(name===ButtonName.STATS && !this.stats.active){
-            cc.tween(this.stats)
-            .call(()=>{
-                this.skill.active = false;
-                this.stats.active = true;
-            })
-            .to(0.1, { opacity: 255 }) 
-            .start();
-        }
-        if(name===ButtonName.SKILL && !this.skill.active){
-            cc.tween(this.skill)
-            .call(()=>{
-                this.stats.active = false
-                this.skill.active = true;
-            })
-            .to(0.1, { opacity: 255 }) 
-            .start();
-        }
-    },
-    activeButton(name){
-        let labelStats = this.statsButton.getChildByName("Label");
-        let spriteStats = this.statsButton.getChildByName("Sprite");
-
-        let labelSkill = this.skillButton.getChildByName("Label");
-        let spriteSkill = this.skillButton.getChildByName("Sprite");
         if (name === ButtonName.STATS && !this.stats.active) {
-            labelSkill.color = cc.Color.GRAY;
-            spriteSkill.getComponent(cc.Sprite).spriteFrame = this.unActiveSpriteFrame;
-
-            labelStats.color = cc.Color.WHITE;
-            spriteStats.getComponent(cc.Sprite).spriteFrame = this.activeSpriteFrame;   
+            cc.tween(this.stats)
+                .call(() => {
+                    this.skill.active = false;
+                    this.stats.active = true;
+                })
+                .to(0.1, { opacity: 255 })
+                .start();
         }
         if (name === ButtonName.SKILL && !this.skill.active) {
-            labelStats.color = cc.Color.GRAY;
-            spriteStats.getComponent(cc.Sprite).spriteFrame = this.unActiveSpriteFrame;
-
-            labelSkill.color = cc.Color.WHITE;
-            spriteSkill.getComponent(cc.Sprite).spriteFrame = this.activeSpriteFrame;   
+            cc.tween(this.skill)
+                .call(() => {
+                    this.stats.active = false
+                    this.skill.active = true;
+                })
+                .to(0.1, { opacity: 255 })
+                .start();
         }
+    },
+    activeButton(name) {
+        const buttons = {
+            [ButtonName.STATS]: this.statsButton,
+            [ButtonName.SKILL]: this.skillButton
+        };
+
+        for (let key in buttons) {
+            const btn = buttons[key];
+            const label = btn.getChildByName("Label");
+            const sprite = btn.getChildByName("Sprite").getComponent(cc.Sprite);
+
+            const isActive = key === name;
+            label.color = isActive ? cc.Color.WHITE : cc.Color.GRAY;
+            sprite.spriteFrame = isActive ? this.activeSpriteFrame : this.unActiveSpriteFrame;
+        }
+    },
+
+    onUpgradeButtonClick(event,typeSkill) {
+
     }
 });
